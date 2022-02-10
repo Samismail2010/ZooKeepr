ac2879b398e419d703a46086f6d5d9ccc0bd9cd4
Samismail2010 <88996409+Samismail2010@users.noreply.github.com>
2022-02-10 03:39:06 -0600

    complete zookeeper section

--- public/assets/js/animals.js
+++ public/assets/js/animals.js
@@ -32,12 +32,12 @@ const getAnimals = (formData = {}) => {
   console.log(queryUrl);
 
   fetch(queryUrl)
-  .then(response => {
-    if(!response.ok) {
-      return alert ('Error: ' + response.statusText);
-    }
-    return response.json();
-  })
+    .then(response => {
+      if (!response.ok) {
+        return alert('Error: ' + response.statusText);
+      }
+      return response.json();
+    })
     .then(animalData => {
       console.log(animalData);
       printResults(animalData);
